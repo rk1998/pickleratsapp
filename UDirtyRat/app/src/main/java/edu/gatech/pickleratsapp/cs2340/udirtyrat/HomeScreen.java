3e9bee8c21b4039ffafddbd8a13ad285f49f3265
Bram Tyler <bramdmtyler@gmail.com>
2017-09-23 17:06:50 -0400

    Bram created the home screen, set it to open on launch, and set it to open the login activity when the login is pressed.

--- UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/HomeScreen.java
+++ UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/HomeScreen.java
@@ -3,11 +3,20 @@ package edu.gatech.pickleratsapp.cs2340.udirtyrat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +24,19 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Locate the button in activity_main.xml
+        login = (Button) findViewById(R.id.login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        // Capture button clicks
+        login.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(HomeScreen.this,
+                        LoginActivity.class);
+                startActivity(myIntent);
+                startActivity(myIntent);
             }
         });
     }
-
 }
