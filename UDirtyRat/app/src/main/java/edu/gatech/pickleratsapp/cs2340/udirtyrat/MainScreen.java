c6a17834cefc28c0e594a701abc71919fde6cce8
Nickdagreat29 <pointguardnick29@gmail.com>
2017-09-26 16:40:27 -0400

    Logout Button Update

--- UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/MainScreen.java
+++ UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/MainScreen.java
@@ -1,13 +1,14 @@
 package edu.gatech.pickleratsapp.cs2340.udirtyrat;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+    private Button logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +16,15 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        logOut = (Button) findViewById(R.id.back_to_home_button);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(fab);
+        logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(MainScreen.this, HomeScreen.class));
             }
         });
     }
