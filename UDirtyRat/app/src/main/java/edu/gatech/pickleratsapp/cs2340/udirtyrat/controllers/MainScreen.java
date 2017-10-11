ccb8e6607e861c6a61abb02e939f391241335fec
rk1998 <rohithk98@gmail.com>
2017-10-10 21:30:33 -0400

    added activity that will hold list view of the rat reports

--- UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/controllers/MainScreen.java
+++ UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/controllers/MainScreen.java
@@ -11,6 +11,7 @@ import edu.gatech.pickleratsapp.cs2340.udirtyrat.*;
 
 public class MainScreen extends AppCompatActivity {
     private Button logOut;
+    private Button viewReports;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         logOut = (Button) findViewById(R.id.back_to_home_button);
+        viewReports = (Button) findViewById(R.id.view_reports_button);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(fab);
         logOut.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +31,14 @@ public class MainScreen extends AppCompatActivity {
                 startActivity(new Intent(MainScreen.this, HomeScreen.class));
             }
         });
+
+        viewReports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainScreen.this, ReportListView.class));
+            }
+        });
+
     }
 
 }
---
+++ UDirtyRat/app/src/main/java/edu/gatech/pickleratsapp/cs2340/udirtyrat/controllers/ReportListView.java
@@ -0,0 +1,15 @@
+package edu.gatech.pickleratsapp.cs2340.udirtyrat.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.pickleratsapp.cs2340.udirtyrat.R;
+
+public class ReportListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_list_view);
+    }
+}
